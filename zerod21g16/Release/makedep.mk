567f1b28e75fedc2bd5d0b226cd37000ec34b2b7
rivargut <55878077+rivargut@users.noreply.github.com>
2022-07-01 13:52:37 -0600

    Add files via upload

---
+++ zerod21g16/Release/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+board_driver_led.c
+
+board_driver_serial.c
+
+board_driver_usb.c
+
+board_init.c
+
+board_startup.c
+
+main.c
+
+sam_ba_cdc.c
+
+sam_ba_monitor.c
+
+sam_ba_serial.c
+
+sam_ba_usb.c
+
